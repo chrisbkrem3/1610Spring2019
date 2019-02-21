fc57d0383b89a08197213ceff17fde021aafb4c5
Unknown <chrisburton.burton3@gmail.com>
2019-02-21 12:40:26 -0700

    Health Power Up

---
+++ 1610FirstGame/Assets/Scripts/HealthEvent.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class HealthEvent : MonoBehaviour
+{
+    public UnityEvent Event;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Event.Invoke();
+    }
+}
---
+++ 1610FirstGame/Assets/Scripts/PowerUpBase.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class PowerUpBase : ScriptableObject
+{
+    public float Value = 10.5f;
+
+    void OnEnable ()
+    {
+
+    }
+
+    public void OnPowerUp(float addValue)
+    {
+        Value += addValue;
+    }
+}
