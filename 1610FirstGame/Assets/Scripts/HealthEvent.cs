9945302602e659a382656ea372a8fab1d6f8ffb3
Unknown <chrisburton.burton3@gmail.com>
2019-02-24 17:54:59 -0700

    Modified Health Script

--- 1610FirstGame/Assets/Scripts/HealthEvent.cs
+++ 1610FirstGame/Assets/Scripts/HealthEvent.cs
@@ -1,11 +1,17 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
 
-public class HealthEvent : MonoBehaviour
+public class HealthEvent : MonoEvents
 {
     public UnityEvent Event;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Event.Invoke();
     }
+
+    void Update()
+    {
+        transform.Rotate(0, 0, 1);
+    }
+
 }
