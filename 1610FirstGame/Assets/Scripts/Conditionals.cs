f0a07a2bb1904bbfc9bc008f6ac7f452b4574292
Unknown <chrisburton.burton3@gmail.com>
2019-02-25 19:04:00 -0700

    Homework Commiting

---
+++ 1610FirstGame/Assets/Scripts/Conditionals.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conditional : MonoBehaviour
+{
+
+    public bool CanWalk;
+    public int Number = 11;
+    public string Password;
+
+
+  
+    void Update()
+    {
+        if (CanWalk)
+        {
+            print("True");
+        }
+
+        if (Number >= 10)
+        {
+            print("Greater");
+        }
+    }
+}
