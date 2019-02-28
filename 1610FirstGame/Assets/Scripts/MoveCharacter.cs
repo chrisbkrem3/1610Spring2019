54ce856b8f138582335b043a37a106dfc5b8fbac
Unknown <chrisburton.burton3@gmail.com>
2019-02-28 15:12:03 -0700

    OnGround Event

--- 1610FirstGame/Assets/Scripts/MoveCharacter.cs
+++ 1610FirstGame/Assets/Scripts/MoveCharacter.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MoveCharacter : MonoBehaviour
 {
+
+    public UnityEvent OnGrounded, OffGrounded;
+
     public float Speed = 3;
 
 
@@ -18,6 +22,15 @@ public class MoveCharacter : MonoBehaviour
 
     void Update()
     {
+        if (Controller.isGrounded)
+        {
+            OnGrounded.Invoke();
+        }
+        else
+        {
+            OffGrounded.Invoke();
+        }
+
         position.y = Input.GetAxis("Vertical")*Speed*Time.deltaTime;
         position.x = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
 
