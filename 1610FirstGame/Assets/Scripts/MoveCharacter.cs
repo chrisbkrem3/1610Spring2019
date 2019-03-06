b3880929f4ca37a0bb3f372d316b2d829fc8c6b6
Unknown <chrisburton.burton3@gmail.com>
2019-03-06 16:57:30 -0700

    Modified Controller

--- 1610FirstGame/Assets/Scripts/MoveCharacter.cs
+++ 1610FirstGame/Assets/Scripts/MoveCharacter.cs
@@ -10,7 +10,6 @@ public class MoveCharacter : MonoBehaviour
 
     public float Speed = 3;
     public float Gravity = -9.81f;
-    public float JumpForce = 30;
 
 
     public CharacterController Controller;
@@ -32,21 +31,17 @@ public class MoveCharacter : MonoBehaviour
             OffGrounded.Invoke();
         }
 
-        if (Controller.isGrounded && Input.GetButton("Jump"))
-        {
-            position.y = JumpForce * Time.deltaTime;
-         }
 
         position.y += Gravity * Time.deltaTime;
 
-        //  if (Input.GetAxis("Vertical") > 0 || Input.GetAxis("Vertical") < 0)
-        //  {
-        //      position.y = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
-        //  }
-        //  else
-        //  {
-        //     position.y = Gravity * Time.deltaTime;
-        // }
+          if (Input.GetAxis("Vertical") > 0 || Input.GetAxis("Vertical") < 0)
+          {
+              position.y = Input.GetAxis("Vertical") * Speed * Time.deltaTime;
+          }
+          else
+          {
+             position.y = Gravity * Time.deltaTime;
+         }
 
 
         position.x = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
